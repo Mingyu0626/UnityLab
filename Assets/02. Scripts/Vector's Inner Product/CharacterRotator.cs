f6f18447e00b81fd5821541d7eab62cfd94e71cf
Mingyu0626 <mg010626@naver.com>
2025-12-04 17:18:51 +0900

    Refactor | CharacterRotator 및 TargetMover 리팩토링

--- Assets/02. Scripts/Vector's Inner Product/CharacterRotator.cs
+++ Assets/02. Scripts/Vector's Inner Product/CharacterRotator.cs
@@ -3,18 +3,24 @@ using UnityEngine;
 public class CharacterRotator : MonoBehaviour
 {
     [Header("Rotation Settings")]
-    [SerializeField] private float rotationSpeed = 100f;
+    [SerializeField] private float _rotationSpeed = 100f;
     
-    void Update()
+    private void Update()
     {
-        // Q, E 키로 캐릭터 회전
+        Rotate();
+    }
+
+    private void Rotate()
+    {
         if (Input.GetKey(KeyCode.Q))
         {
-            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.up, -_rotationSpeed * Time.deltaTime);
+            return;
         }
         if (Input.GetKey(KeyCode.E))
         {
-            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.up, _rotationSpeed * Time.deltaTime);
+            return;
         }
     }
 }
--- Assets/02. Scripts/Vector's Inner Product/TargetMover.cs
+++ Assets/02. Scripts/Vector's Inner Product/TargetMover.cs
@@ -3,16 +3,24 @@ using UnityEngine;
 public class TargetMover : MonoBehaviour
 {
     [Header("Movement Settings")]
-    [SerializeField] private float moveSpeed = 2f;
-    [SerializeField] private float rotationSpeed = 100f;
-    
-    void Update()
+    [SerializeField] private float _moveSpeed = 2f;
+
+    public enum AxisName
     {
-        // WASD 키로 타겟 이동
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        
-        Vector3 movement = new Vector3(horizontal, 0, vertical) * moveSpeed * Time.deltaTime;
+        Horizontal,
+        Vertical
+    }
+    
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxis(nameof(AxisName.Horizontal));
+        float vertical = Input.GetAxis(nameof(AxisName.Vertical));
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * _moveSpeed * Time.deltaTime;
         transform.position += movement;
     }
 }
