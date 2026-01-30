3771d173b8aafa1272793c2cf1d14cd8725214dd
Mingyu0626 <mg010626@naver.com>
2026-01-30 13:16:29 +0900

    Test | 테스트 메서드 Mock 클래스 바깥으로 이동

--- Assets/02. Scripts/TestRunner Practice/TestsPlayMode/LevelSystemTests.cs
+++ Assets/02. Scripts/TestRunner Practice/TestsPlayMode/LevelSystemTests.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class LevelSystemTests : MonoBehaviour
+public class LevelSystemTests
 {
     public class DataSaverMock : IDataSaver
     {
@@ -18,32 +18,32 @@ public class LevelSystemTests : MonoBehaviour
             LastSavedValue = value;
             Debug.Log($"MockDataSaver의 Save 메서드 호출됨(실제로 데이터가 저장되지는 않음)");
         }
+    }
 
-        [Test]
-        public void LevelUp_SaveDatas_UsingMock()
-        {
-            // 1. Arrange
-            GameObject go = new GameObject();
-            LevelSystem levelSystem = go.AddComponent<LevelSystem>();
+    [Test]
+    public void LevelUp_SaveDatas_UsingMock()
+    {
+        // 1. Arrange
+        GameObject go = new GameObject();
+        LevelSystem levelSystem = go.AddComponent<LevelSystem>();
 
-            // 진짜 GameDataSaver 대신 DataSaverMock을 주입한다.
-            DataSaverMock mockSaver = new DataSaverMock();
-            levelSystem.Initialize(mockSaver);
+        // 진짜 GameDataSaver 대신 DataSaverMock을 주입한다.
+        DataSaverMock mockSaver = new DataSaverMock();
+        levelSystem.Initialize(mockSaver);
 
-            // 2. Act
-            levelSystem.LevelUp();
+        // 2. Act
+        levelSystem.LevelUp();
 
-            // 3. Assert
-            // 3-1. 레벨업 확인
-            Assert.AreEqual(2, levelSystem.Level);
+        // 3. Assert
+        // 3-1. 레벨업 확인
+        Assert.AreEqual(2, levelSystem.Level);
 
-            // 3-2. 데이터 저장 여부 확인
-            Assert.IsTrue(mockSaver.IsSaveCalled, $"Save 메서드가 호출되지 않음");
-            Assert.AreEqual("PlayerLevel", mockSaver.LastSavedKey);
-            Assert.AreEqual(2, mockSaver.LastSavedValue);
+        // 3-2. 데이터 저장 여부 확인
+        Assert.IsTrue(mockSaver.IsSaveCalled, $"Save 메서드가 호출되지 않음");
+        Assert.AreEqual("PlayerLevel", mockSaver.LastSavedKey);
+        Assert.AreEqual(2, mockSaver.LastSavedValue);
 
-            // Cleanup
-            DestroyImmediate(go);
-        }
+        // TearDown
+        Object.DestroyImmediate(go);
     }
 }
